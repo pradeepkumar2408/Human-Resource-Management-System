7f286cadc0a91f9afaf391fcf19658c64463eaad
jai <jaisurya7482@gmal.com>
2026-08-22 11:48:53 +0530

    Login

--- front-end/my-react-app/src/App.jsx
+++ front-end/my-react-app/src/App.jsx
@@ -3,14 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Checking from './Components/Checking'
+import Login from './Components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Checking/>
+      <Login />
     </>
   )
 }
