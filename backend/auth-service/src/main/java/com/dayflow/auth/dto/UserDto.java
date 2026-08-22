7aa9911b472263b15d4e8f42641cd28beb0fc447
Saisaran_p146 <saisaran158@gmail.com>
2026-08-22 12:41:18 +0530

    auth service created

---
+++ backend/auth-service/src/main/java/com/dayflow/auth/dto/UserDto.java
@@ -0,0 +1,19 @@
+package com.dayflow.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private String department;
+}
